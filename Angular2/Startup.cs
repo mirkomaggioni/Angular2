bb89979795fb21fe3fd71f2b1918f1ee93764c7b
Mirko Maggioni <mirko.maggioni@gmail.com>
2016-10-22 17:28:16 +0200

    Add project files.

---
+++ Angular2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Angular2.Startup))]
+
+namespace Angular2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
