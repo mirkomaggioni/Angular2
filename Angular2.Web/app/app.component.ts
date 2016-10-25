28ade6e8d0660866a32ec6d879bfdee930f69dd5
Mirko Maggioni <mirko.maggioni@gmail.com>
2016-10-25 18:07:25 +0200

    Adding gulpfile Adding basic components

---
+++ Angular2.Web/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    templateUrl: "/app/views/app.html"
+})
+
+export class AppComponent {
+
+}
---
+++ Angular2.Web/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { ToastModule } from "ng2-toastr/ng2-toastr";
+
+import { AppComponent } from "../app/app.component";
+import { CustomersComponent } from "../app/components/customers";
+import { InvoicesComponent } from "../app/components/invoices";
+
+@NgModule ({
+    imports: [ 
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { 
+                path: "",
+                redirectTo: "invoices",
+                pathMatch: "full" 
+            },
+            { 
+                path: "customers",
+                component: CustomersComponent  
+            },
+            { 
+                path: "invoices",
+                component: InvoicesComponent
+            }
+        ]),
+        HttpModule,
+        ToastModule
+    ],
+    declarations: [
+        AppComponent,
+        CustomersComponent,
+        InvoicesComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+
+export class AppModule {}
