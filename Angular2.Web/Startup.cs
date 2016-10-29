150f99d5a887551896db6cb23db3b1fe561e9dc6
Mirko Maggioni <mirko.maggioni@gmail.com>
2016-10-29 16:46:47 +0200

    Adding Core library Adding Owin configuration for WebApi

--- Angular2.Web/Startup.cs
+++ Angular2.Web/Startup.cs
@@ -5,25 +5,18 @@ using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
 
-[assembly: OwinStartup(typeof(Angular2.Startup))]
+[assembly: OwinStartup(typeof(Angular2.Web.Startup))]
 
-namespace Angular2
+namespace Angular2.Web
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
-            //GlobalConfiguration.Configure(c => WebApiConfig.Register(config));
-            //app.UseWebApi(config);
+   
+            GlobalConfiguration.Configure(c => WebApiConfig.Register(config));
+            app.UseWebApi(config);
         }
     }
 }
